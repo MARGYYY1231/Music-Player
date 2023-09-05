f693032d8dbcaa335a0dafb3ce82d658a56426d1
Margaret Nieves Gonzales <138099922+MARGYYY1231@users.noreply.github.com>
2023-09-05 22:28:23 +1200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+/*
+Boom Clap - Nightcore Wolfie
+Maps - Mirima
+Bring me to Life - Sinnon
+Down - Sinnon
+Alphabet Boy Mashup - Teddie
+Cold Water - Sinnon
+
+Song 1:
+https://youtu.be/i7RePRNMXx4?si=uHrqE2GhX-f-HkU8
+
+Song 2:
+https://youtu.be/CwugFsGdHjU?si=U7GhoCKvKXOj66BJ
+
+Song 3:
+https://youtu.be/HiU874Aa65U?si=uVMGWRS4s33DhiN7
+
+Song 4:
+https://youtu.be/D5TuhKC7ATs?si=_m1yGwxkQLjw-fhq
+
+Song 5:
+https://youtu.be/RDT4bHUs8VQ?si=pls-7itieYeTAp2_
+
+Song 6:
+https://youtu.be/t483ltJR5IE?si=HurMxzZO0R9e-RMn
+
+Image src:
+1.png:
+https://yt3.googleusercontent.com
+/d27rP1Ik9cSbuOeLy4NvqQx4toD2KxAz5bY22qk9PbTszD_OND52q317rFBCBavqoylDbYeTTQ=s900-c-k-c0x00ffffff-no-rj
+
+2.png:
+https://yt3.googleusercontent.com/ytc/AOPolaTQw4flRwoq_kl-syvceMkv6MqkICCm5l6W4U9C=s176-c-k-c0x00ffffff-no-rj-mo
+
+3.png:
+https://i.scdn.co/image/ab67616d00001e02e10bf848ed63de6513559354
+
+4.png:
+https://yt3.googleusercontent.com
+/c1SLxir_09oYuaBT3peUE077R2YbPwMlVbsVkkMf24FzRkZZJ74pi3BHeJGNRn9wFguWZG2u=s900-c-k-c0x00ffffff-no-rj
+
+5.png:
+https://yt3.googleusercontent.com/ytc/AOPolaR8zA1wJWQNk3cKtXgIyXxufJDE7HYlsBvohCDu=s900-c-k-c0x00ffffff-no-rj
+
+6.png:
+https://yt3.googleusercontent.com/ytc/AOPolaSPYpFAXVHvyqy3zIf6jUewMskyOaNEsehDjRYr=s900-c-k-c0x00ffffff-no-rj
+
+*/
+
+let songs = [
+    {
+        name: 'Bring Me to Life - Nightcore',
+        path: 'music/BringMeToLife.mp3',
+        artist: 'Sinnon',
+        cover: 'images/1.png'
+    },
+    {
+        name: 'Down - Nightcore',
+        path: 'music/Down.mp3',
+        artist: 'Sinnon',
+        cover: 'images/2.png'
+    },
+    {
+        name: 'Cold Water Mashup - Nightcore',
+        path: 'music/Mashup2.mp3',
+        artist: 'Sinnon',
+        cover: 'images/3.png'
+    },
+    {
+        name: 'Alphabet Boy Mashup - Nightcore',
+        path: 'music/Mashup1.mp3',
+        artist: 'Teddie',
+        cover: 'images/4.png'
+    },
+    {
+        name: 'Maps - Nightcore',
+        path: 'music/Maps.mp3',
+        artist: 'Mirima',
+        cover: 'images/5.png'
+    },
+    {
+        name: 'Boom Clap - Nightcore',
+        path: 'music/BoomClap.mp3',
+        artist: 'Nightcore Wolfie',
+        cover: 'images/6.png'
+    },
+]
